799b825899a88b8feec96465b93e8817c0a48bc4
gang89liu <asd6224868@hotmail.com>
2020-03-30 23:10:13 +0800

    底部导航栏

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart' show Constants, AppColors, AppStyles;
+
+import '../i18n/strings.dart' show Strings;
+
+class NavigationIconView {
+  final BottomNavigationBarItem item;
+
+  NavigationIconView(
+      {Key key, String title, IconData icon, IconData activeIcon})
+      : item = BottomNavigationBarItem(
+          icon: Icon(icon),
+          activeIcon: Icon(activeIcon),
+          title: Text(title),
+          backgroundColor: Colors.white,
+        );
+}
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<NavigationIconView> _navigationViews;
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationViews = [
+      NavigationIconView(
+        title: Strings.TitleWechat,
+        icon: IconData(
+          0xe608,
+          fontFamily: Constants.IconFontFamily,
+        ),
+        activeIcon: IconData(
+          0xe603,
+          fontFamily: Constants.IconFontFamily,
+        ),
+      ),
+      NavigationIconView(
+          title: Strings.TitleContact,
+          icon: IconData(
+            0xe601,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          activeIcon: IconData(
+            0xe656,
+            fontFamily: Constants.IconFontFamily,
+          )),
+      NavigationIconView(
+          title: Strings.TitleDiscovery,
+          icon: IconData(
+            0xe600,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          activeIcon: IconData(
+            0xe671,
+            fontFamily: Constants.IconFontFamily,
+          )),
+      NavigationIconView(
+          title: Strings.TitleMe,
+          icon: IconData(
+            0xe6c0,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          activeIcon: IconData(
+            0xe626,
+            fontFamily: Constants.IconFontFamily,
+          )),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final BottomNavigationBar botNavBar = BottomNavigationBar(
+      items: _navigationViews.map<BottomNavigationBarItem>((NavigationIconView view) {
+        return view.item;
+      }).toList(),
+      currentIndex: 0,
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        print('点击的是第$index个Tab');
+      },
+    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('微信'),
+        ),
+        body: Container(color: Colors.red),
+        bottomNavigationBar: botNavBar,
+        );
+  }
+}
